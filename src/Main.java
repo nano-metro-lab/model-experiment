806a3a042d8315a7cf00a8feba87789d0b19019e
exuanbo <exuanbo@protonmail.com>
2023-01-23 21:08:24 +0000

    feat: add basic demo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import model.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Station stationCircle = new Station(StationType.CIRCLE);
+    Station stationSquare = new Station(StationType.SQUARE);
+    Station stationTriangle1 = new Station(StationType.TRIANGLE);
+    Station stationTriangle2 = new Station(StationType.TRIANGLE);
+
+    new Passenger(stationCircle, StationType.TRIANGLE);
+
+    Line lineCircleToSquare = new Line(stationCircle, stationSquare);
+    Line lineSquareToTriangle1 = new Line(stationSquare, stationTriangle1);
+    Line lineSquareToTriangle2 = new Line(stationSquare, stationTriangle2);
+
+    Train trainCircleToSquare = new Train(lineCircleToSquare, stationCircle);
+    Train trainSquareToTriangle1 = new Train(lineSquareToTriangle1, stationSquare);
+    Train trainSquareToTriangle2 = new Train(lineSquareToTriangle2, stationSquare);
+
+    trainCircleToSquare.start();
+    assert stationCircle.getPassengers().length == 0;
+    assert trainCircleToSquare.getPassengers().length > 0;
+
+    trainCircleToSquare.stop();
+    assert trainCircleToSquare.getPassengers().length == 0;
+    assert stationSquare.getPassengers().length > 0;
+
+    trainSquareToTriangle1.start();
+    assert stationSquare.getPassengers().length == 0;
+    assert trainSquareToTriangle1.getPassengers().length > 0;
+
+    trainSquareToTriangle1.stop();
+    assert trainSquareToTriangle1.getPassengers().length == 0;
+    assert stationSquare.getPassengers().length == 0;
+
+    trainCircleToSquare.start();
+    trainCircleToSquare.stop();
+
+    new Passenger(stationCircle, StationType.TRIANGLE);
+
+    trainCircleToSquare.start();
+    trainCircleToSquare.stop();
+
+    trainSquareToTriangle2.start();
+    assert trainSquareToTriangle2.getPassengers().length > 0;
+
+    trainSquareToTriangle2.stop();
+    assert trainSquareToTriangle2.getPassengers().length == 0;
   }
 }
